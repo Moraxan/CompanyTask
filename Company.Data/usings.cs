a859159e4cdcb921cf67e01f1ad3247a3d6d02a7
Moraxan <calletrumma@gmail.com>
2022-12-23 11:58:02 +0100

    Add project files.

---
+++ Company.Data/usings.cs
@@ -0,0 +1,11 @@
+﻿global using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Data;
+
+internal class usings
+{
+}
