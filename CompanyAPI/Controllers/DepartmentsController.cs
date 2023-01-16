e720484c2ffdee7c2cd8ded688df072366de673f
Moraxan <calletrumma@gmail.com>
2023-01-16 20:05:16 +0100

    Nu går det CRUD:a på alla entities utom LinkPosition

--- CompanyAPI/Controllers/DepartmentsController.cs
+++ CompanyAPI/Controllers/DepartmentsController.cs
@@ -1,4 +1,5 @@
-﻿using Company.Data.Interfaces;
+﻿using Company.Common.DTOs;
+using Company.Data.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -7,10 +8,10 @@ namespace CompanyAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CompanysController : ControllerBase
+    public class DepartmentsController : ControllerBase
     {
         private readonly IDbService _db;
-        public CompanysController(IDbService db)
+        public DepartmentsController(IDbService db)
         {
             _db = db;
         }
@@ -18,7 +19,7 @@ namespace CompanyAPI.Controllers
         [HttpGet]
         public async Task<IResult> Get()
         {
-            var companys = await _db.GetAsync<CompanyName, CompanyNameDTO>();
+            var companys = await _db.GetAsync<Department, DepartmentDTO>();
             return Results.Ok(companys);
         }
 
@@ -26,19 +27,19 @@ namespace CompanyAPI.Controllers
         [HttpGet("{id}")]
         public async Task<IResult> Get(int id)
         {
-            var company = await _db.SingleAsync<CompanyName, CompanyNameDTO>(c => c.Id.Equals(id));
+            var company = await _db.SingleAsync<Department, DepartmentDTO>(c => c.Id.Equals(id));
             return Results.Ok(company);
         }
 
         // POST api/<CompanysController>
         [HttpPost]
-        public async Task<IResult> Post([FromBody] CompanyNameDTO dto)
+        public async Task<IResult> Post([FromBody] DepartmentDTO dto)
         {
             if (dto is null) return Results.BadRequest();
-            var company = await _db.AddAsync<CompanyName, CompanyNameDTO>(dto);
+            var company = await _db.AddAsync<Department, DepartmentDTO>(dto);
             if (await _db.SaveChangesAsync())
             {
-                var Uri = _db.GetURI<CompanyName>(company);
+                var Uri = _db.GetURI<Department>(company);
                 return Results.Created(Uri, company);
             }
             return Results.BadRequest();
@@ -46,14 +47,14 @@ namespace CompanyAPI.Controllers
 
         // PUT api/<CompanysController>/5
         [HttpPut("{id}")]
-        public async Task<IResult> Put(int id, [FromBody] CompanyNameDTO dto)
+        public async Task<IResult> Put(int id, [FromBody] DepartmentDTO dto)
         {
             try
             {
 
-                if (!await _db.AnyAsync<CompanyName>(e => e.Id.Equals(id)))
+                if (!await _db.AnyAsync<Department>(e => e.Id.Equals(id)))
                     return Results.NotFound();
-                _db.Update<CompanyName, CompanyNameDTO>(id, dto);
+                _db.Update<Department, DepartmentDTO>(id, dto);
                 if (await _db.SaveChangesAsync()) return Results.NoContent();
 
 
@@ -61,28 +62,28 @@ namespace CompanyAPI.Controllers
             catch (Exception ex)
             {
 
-                return Results.BadRequest($"Couldn't update the {typeof(CompanyName).Name}entity.\n{ex}.");
+                return Results.BadRequest($"Couldn't update the {typeof(Department).Name}entity.\n{ex}.");
 
             }
             return Results.BadRequest();
-            
+
 
         }
 
         // DELETE api/<CompanysController>/5
         [HttpDelete("{id}")]
-       
+
 
         public async Task<IResult> Delete(int id)
         {
             try
             {
-                if (!await _db.DeleteAsync<CompanyName>(id)) return Results.NotFound();
+                if (!await _db.DeleteAsync<Department>(id)) return Results.NotFound();
                 if (await _db.SaveChangesAsync()) return Results.NoContent();
             }
             catch (Exception ex)
             {
-                return Results.BadRequest($"Couldn't delete the {typeof(CompanyName).Name}entity.\n{ex}.");
+                return Results.BadRequest($"Couldn't delete the {typeof(Department).Name}entity.\n{ex}.");
 
                 throw;
             }
--- CompanyAPI/Controllers/EmployeesController.cs
+++ CompanyAPI/Controllers/EmployeesController.cs
@@ -7,10 +7,10 @@ namespace CompanyAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CompanysController : ControllerBase
+    public class EmployeesController : ControllerBase
     {
         private readonly IDbService _db;
-        public CompanysController(IDbService db)
+        public EmployeesController(IDbService db)
         {
             _db = db;
         }
@@ -18,7 +18,7 @@ namespace CompanyAPI.Controllers
         [HttpGet]
         public async Task<IResult> Get()
         {
-            var companys = await _db.GetAsync<CompanyName, CompanyNameDTO>();
+            var companys = await _db.GetAsync<Employee, EmployeeDTO>();
             return Results.Ok(companys);
         }
 
@@ -26,19 +26,19 @@ namespace CompanyAPI.Controllers
         [HttpGet("{id}")]
         public async Task<IResult> Get(int id)
         {
-            var company = await _db.SingleAsync<CompanyName, CompanyNameDTO>(c => c.Id.Equals(id));
+            var company = await _db.SingleAsync<Employee, EmployeeDTO>(c => c.Id.Equals(id));
             return Results.Ok(company);
         }
 
         // POST api/<CompanysController>
         [HttpPost]
-        public async Task<IResult> Post([FromBody] CompanyNameDTO dto)
+        public async Task<IResult> Post([FromBody] EmployeeDTO dto)
         {
             if (dto is null) return Results.BadRequest();
-            var company = await _db.AddAsync<CompanyName, CompanyNameDTO>(dto);
+            var company = await _db.AddAsync<Employee, EmployeeDTO>(dto);
             if (await _db.SaveChangesAsync())
             {
-                var Uri = _db.GetURI<CompanyName>(company);
+                var Uri = _db.GetURI<Employee>(company);
                 return Results.Created(Uri, company);
             }
             return Results.BadRequest();
@@ -46,14 +46,14 @@ namespace CompanyAPI.Controllers
 
         // PUT api/<CompanysController>/5
         [HttpPut("{id}")]
-        public async Task<IResult> Put(int id, [FromBody] CompanyNameDTO dto)
+        public async Task<IResult> Put(int id, [FromBody] EmployeeDTO dto)
         {
             try
             {
 
-                if (!await _db.AnyAsync<CompanyName>(e => e.Id.Equals(id)))
+                if (!await _db.AnyAsync<Employee>(e => e.Id.Equals(id)))
                     return Results.NotFound();
-                _db.Update<CompanyName, CompanyNameDTO>(id, dto);
+                _db.Update<Employee, EmployeeDTO>(id, dto);
                 if (await _db.SaveChangesAsync()) return Results.NoContent();
 
 
@@ -61,28 +61,28 @@ namespace CompanyAPI.Controllers
             catch (Exception ex)
             {
 
-                return Results.BadRequest($"Couldn't update the {typeof(CompanyName).Name}entity.\n{ex}.");
+                return Results.BadRequest($"Couldn't update the {typeof(Employee).Name}entity.\n{ex}.");
 
             }
             return Results.BadRequest();
-            
+
 
         }
 
         // DELETE api/<CompanysController>/5
         [HttpDelete("{id}")]
-       
+
 
         public async Task<IResult> Delete(int id)
         {
             try
             {
-                if (!await _db.DeleteAsync<CompanyName>(id)) return Results.NotFound();
+                if (!await _db.DeleteAsync<Employee>(id)) return Results.NotFound();
                 if (await _db.SaveChangesAsync()) return Results.NoContent();
             }
             catch (Exception ex)
             {
-                return Results.BadRequest($"Couldn't delete the {typeof(CompanyName).Name}entity.\n{ex}.");
+                return Results.BadRequest($"Couldn't delete the {typeof(Employee).Name}entity.\n{ex}.");
 
                 throw;
             }
